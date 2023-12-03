bcefc7cd18b55552c599ca850af7a81ec5d496cb
asix178 <joannakarasinska0@gmail.com>
2023-12-04 00:07:08 +0100

    Added Calculator

--- src/main/java/org/asia/Calculator.java
+++ src/main/java/org/asia/Calculator.java
@@ -1,2 +1,57 @@
-package org.asia;public class Calculator {
+package org.asia;
+
+public class Calculator {
+    private Integer number1;
+    private Integer number2;
+
+    public Calculator(Integer number1, Integer number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public Integer getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(Integer number1) {
+        this.number1 = number1;
+    }
+
+    public Integer getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(Integer number2) {
+        this.number2 = number2;
+    }
+
+    public Integer addNumbers(){
+        return number1+number2;
+    }
+
+    public Integer subtractNumbers(){
+        return number1-number2;
+    }
+
+    public Integer multiplyNumbers(){
+        return number1*number2;
+    }
+
+    public Float divideNumbers(){
+        return ((float)number1/(float)number2);
+    }
+
+    public Integer powerNumbers(){
+        return (int) Math.pow(number1,number2);
+    }
+
+    /**
+     * Daje a bo tak
+     * @param a nie wiem
+     * @param b tez nie wiem
+     * @return a
+     */
+    public Integer adam(Integer a, Integer b){
+        return a;
+    }
 }
--- src/test/java/org/asia/CalculatorTest.java
+++ src/test/java/org/asia/CalculatorTest.java
@@ -1,2 +1,20 @@
-package org.asia;public class CalculatorTest {
+package org.asia;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    public void calculatorAddTest(){
+        Calculator Steve = new Calculator(1,2);
+        assertEquals(3,Steve.addNumbers());
+    }
+    @Test
+    public void calculatorPowerTest(){
+        Calculator Roger = new Calculator(2,3);
+        assertEquals(8,Roger.powerNumbers());
+        Roger.adam(2,3);
+    }
 }
